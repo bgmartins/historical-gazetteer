45425b53c31ec0f1a26af84240636ea4244b3996
Bruno Martins <bgmartins@bruno-tagus>
2018-05-09 14:16:28 +0100

    Added script to generate ER diagram from database.

---
+++ generate-schema.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Download schema crawler
+mkdir temp
+cd temp
+wget https://github.com/schemacrawler/SchemaCrawler/releases/download/v14.20.06/schemacrawler-14.20.06-distribution.zip
+unzip schemacrawler-14.20.06-distribution.zip
+
+# The path of the unzipped SchemaCrawler directory
+SchemaCrawlerPATH=./schemacrawler-14.20.06-distribution
+
+# The path of the SQLite database
+SQLiteDatabaseFILE=../gazetteer.db
+
+# The type of the database system.
+RDBMS=sqlite
+
+# Where to store the image
+OutputPATH=../ER-diagram.pdf
+
+# Username and password need to be empty for SQLite
+USER=
+PASSWORD=
+ 
+# Generate diagram
+java -classpath $(echo ${SchemaCrawlerPATH}/_schemacrawler/lib/*.jar | tr ' ' ':') schemacrawler.Main -server=${RDBMS} -database=${SQLiteDatabaseFILE} -outputformat=pdf -outputfile=${OutputPATH} -command=details -routines= -tabletypes=TABLE -infolevel=maximum -user=${USER} -password=${PASSWORD} -loglevel=CONFIG
+echo "Finished generating the diagram"
+
+# Remove schema crawler
+cd ..
+rm -rf temp
