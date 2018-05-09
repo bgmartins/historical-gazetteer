b29fd4446c05fd339dd42910bb731e45f2663bf6
Jacinto Estima <jacinto.estima@gmail.com>
2018-05-10 00:56:22 +0100

    import-tng.py

--- import-tng.py
+++ import-tng.py
@@ -4,16 +4,21 @@ import sqlite3
 conn = sqlite3.connect("test.sqlite")
 c = conn.cursor()
 
-with open("export.xml") as fd: obj = xmltodict.parse(fd.read())
+with open("tgn1-extract.xml") as fd: obj = xmltodict.parse(fd.read())
+
+#print (obj["Vocabulary"]["Subject"])
 
 for row in obj["Vocabulary"]["Subject"]:
 	id = row["@Subject_ID"]
-	preferred_name = row["Terms"]["Preferred_Term"]
-	latitude = row["Coordinates"]["Latitude"]["Decimal"]
-	longitude = row["Coordinates"]["Longitude"]["Decimal"]
+	if "Associative_Relationships" in row:
+		print (row["Associative_Relationships"]["Associative_Relationship"])
+		#if "Description" in row:
+	#preferred_name = row["Terms"]["Preferred_Term"]
+	#latitude = row["Coordinates"]["Latitude"]["Decimal"]
+	#longitude = row["Coordinates"]["Longitude"]["Decimal"]
 	
-	parent = row["Preferred_Parent"]["Parent_Subject_ID"]
-	parent_relation = row["Preferred_Parent"]["Parent_Subject_ID"]
+	#parent = row["Preferred_Parent"]["Parent_Subject_ID"]
+	#parent_relation = row["Preferred_Parent"]["Parent_Subject_ID"]
 	
 	#Vocabulary/Subject/Coordinates/Bounding/Latitude_Least
 	#Vocabulary/Subject/Coordinates/Bounding/Latitude_Most
@@ -21,6 +26,6 @@ for row in obj["Vocabulary"]["Subject"]:
 	#Vocabulary/Subject/Coordinates/Bounding/Longitude_Most
 	#Vocabulary/Subject/Coordinates/Elevation_Meters
 	#Vocabulary/Subject/Coordinates/Elevation_Feet
-    for column in row["column"]:
-        c.execute("INSERT INTO table VALUES '(?,?)'", [column["@name"], column["#text"]])
-    print "item inserted \n"
+	#for column in row["column"]:
+        #c.execute("INSERT INTO table VALUES '(?,?)'", [column["@name"], column["#text"]])
+		#print ("item inserted \n")
