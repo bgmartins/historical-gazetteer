6805065253dc4d49f0c869aee3172368703da215
Jacinto Estima <jacinto.estima@gmail.com>
2018-05-10 17:56:00 +0100

    code to extract Associative Relationships added to the script
    import-tng.py

--- import-tng.py
+++ import-tng.py
@@ -5,14 +5,14 @@ import sqlite3
 ## function to verify if a certain key exists and retrieve the value
 def verify_key(root, key):
 	if key in root:
-		return len(root[key])
+		return root[key]
 	else:
-		return "false"
+		return ""
 
 conn = sqlite3.connect("test.sqlite")
 c = conn.cursor()
 
-with open("tgn1-extract.xml", encoding='utf-8') as fd: obj = xmltodict.parse(fd.read(), encoding='utf-8', force_list={'Associative_Relationship':True})
+with open("tgn1-extract.xml", encoding='utf-8') as fd: obj = xmltodict.parse(fd.read(), encoding='utf-8', force_list={'Associative_Relationship':True, 'AR_Date':True})
 
 #print (obj["Vocabulary"]["Subject"])
 
@@ -25,9 +25,27 @@ for row in obj["Vocabulary"]["Subject"]:
 	if "Associative_Relationships" in row:
 		#print(row)
 		for rel in row["Associative_Relationships"]["Associative_Relationship"]:
-			print (rel)
-			#print (row["Associative_Relationships"])
-			#print (rel["Historic_Flag"])
+			historic_flag = verify_key(rel, "Historic_Flag")
+			print (historic_flag)
+			description = verify_key(rel, "Description")
+			print (description)
+			if "AR_Date" in rel:
+				for ar_date in rel["AR_Date"]:
+					#print (ar_date)
+					ar_display_date = verify_key(ar_date, "Display_Date")
+					print (ar_display_date)
+					ar_start_date = verify_key(ar_date, "Start_Date")
+					print (ar_start_date)
+					ar_end_date = verify_key(ar_date, "End_Date")
+					print (ar_end_date)
+			relatioship_type = verify_key(rel, "Relationship_Type")
+			print (relatioship_type)
+			if "Related_Subject_ID" in rel:
+				for rel_sub in rel["Related_Subject_ID"]:
+					vp_subject_id = verify_key(ar_date, "VP_Subject_ID")
+					print (vp_subject_id)
+					contrib_subject_id = verify_key(ar_date, "Contrib_Subject_ID")
+					print (contrib_subject_id)
 	
 	#if "Associative_Relationships" in row:
 		#print (row["Associative_Relationships"]["Associative_Relationship"])
