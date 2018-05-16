7857f7eedf9bdcb3f9907412afb0b0f48e4ad460
Jacinto Estima <jacinto.estima@gmail.com>
2018-05-16 15:45:13 +0100

    code to insert a new collection or select the id of an existing collection added
    import-tng.py

--- import-tng.py
+++ import-tng.py
@@ -2,143 +2,164 @@ import xmltodict
 import sqlite3
 #import defusedexpat
 
+## initial parameters and global variables
+new_collection = "no"
+collection_name = "TGN"
+collection_id = ""
+
 ## function to verify if a certain key exists and retrieve the value
 def verify_key(root, key):
-	if key in root:
-		return root[key]
-	else:
-		return ""
+    if key in root:
+        return root[key]
+    else:
+        return ""
 
-conn = sqlite3.connect("test.sqlite")
+def get_latest_id(table, id_field):
+    c.execute("select {fld} from {tbl} order by {fld} desc limit 1;".format(tbl=table,fld=id_field))
+    return c.fetchone()[0]
+
+conn = sqlite3.connect("test.sqlite3")
 c = conn.cursor()
 
+## Lest's start by creating a collection to refer to the source of features
+## inserting a new collection and getting its id
+if new_collection == "yes":
+    collection_id = get_latest_id("g_collection", "collection_id")
+    new_collection_id = collection_id + 1
+    #print (new_collection_id)
+    c.execute("INSERT INTO {tbl} (collection_id, name, note) VALUES (:col_id , :nm, :nt);".format(tbl = "g_collection"), {'col_id': new_collection_id, 'nm': collection_name , 'nt': '\"collection of features from TGN\"'})
+    conn.commit()
+    print("collection " + collection_name + " added")
+elif new_collection == "no":
+    c.execute("SELECT collection_id from g_collection where name ==:nm",{'nm':collection_name})
+    collection_id = (c.fetchone()[0])
+
+
+### Now we open the XML file to import to the gazeteer database
 with open("tgn1-extract.xml", encoding='utf-8') as fd: obj = xmltodict.parse(fd.read(), encoding='utf-8', force_list={'Associative_Relationship':True, 'AR_Date':True, 'Coordinates':True})
 
 #print (obj["Vocabulary"]["Subject"])
 
 for row in obj["Vocabulary"]["Subject"]:
-	#print (type(row))
-	
-	feature = row["@Subject_ID"] #id of the subject
-	print (feature)
-	
+    #print (type(row))
+
+    feature = row["@Subject_ID"] #id of the subject
+    #print (feature)
+
 ###
 ## To extract Associative Relationaships if they exist
-	if "Associative_Relationships" in row:
-		#print(row)
-		for rel in row["Associative_Relationships"]["Associative_Relationship"]:
-			historic_flag = verify_key(rel, "Historic_Flag")
-			#print (historic_flag)
-			description = verify_key(rel, "Description")
-			#print (description)
-			if "AR_Date" in rel:
-				for ar_date in rel["AR_Date"]:
-					#print (ar_date)
-					ar_display_date = verify_key(ar_date, "Display_Date")
-					#print (ar_display_date)
-					ar_start_date = verify_key(ar_date, "Start_Date")
-					#print (ar_start_date)
-					ar_end_date = verify_key(ar_date, "End_Date")
-					#print (ar_end_date)
-			relatioship_type = verify_key(rel, "Relationship_Type")
-			#print (relatioship_type)
-			if "Related_Subject_ID" in rel:
-				for rel_sub in rel["Related_Subject_ID"]:
-					vp_subject_id = verify_key(ar_date, "VP_Subject_ID")
-					#print (vp_subject_id)
-					contrib_subject_id = verify_key(ar_date, "Contrib_Subject_ID")
-					#print (contrib_subject_id)
-					
-"""
-To extract Coordinates if they exist
+    if "Associative_Relationships" in row:
+        #print(row)
+        for rel in row["Associative_Relationships"]["Associative_Relationship"]:
+            historic_flag = verify_key(rel, "Historic_Flag")
+            #print (historic_flag)
+            description = verify_key(rel, "Description")
+            #print (description)
+            if "AR_Date" in rel:
+                for ar_date in rel["AR_Date"]:
+                    #print (ar_date)
+                    ar_display_date = verify_key(ar_date, "Display_Date")
+                    #print (ar_display_date)
+                    ar_start_date = verify_key(ar_date, "Start_Date")
+                    #print (ar_start_date)
+                    ar_end_date = verify_key(ar_date, "End_Date")
+                    #print (ar_end_date)
+            relatioship_type = verify_key(rel, "Relationship_Type")
+            #print (relatioship_type)
+            if "Related_Subject_ID" in rel:
+                for rel_sub in rel["Related_Subject_ID"]:
+                    vp_subject_id = verify_key(ar_date, "VP_Subject_ID")
+                    #print (vp_subject_id)
+                    contrib_subject_id = verify_key(ar_date, "Contrib_Subject_ID")
+                    #print (contrib_subject_id)
 
-The structure is as follows
+## To extract Coordinates if they exist
 
-Coordinates/Standard/Latitude/Decimal						mapped to	g_location	north_coordinate + south_coordinate
-Coordinates/Standard/Longitude/Decimal						mapped to	g_location	east_coordinate + west_coordinate
-Coordinates/Bounding/Latitude/Latitude_Least/Decimal		mapped to	g_location	south_coordinate
-Coordinates/Bounding/Latitude/Latitude_Most/Decimal			mapped to	g_location	north_coordinate
-Coordinates/Bounding/Longitude/Longitude_Least/Decimal		mapped to	g_location	west_coordinate
-Coordinates/Bounding/Longitude/Longitude_Most/Decimal		mapped to	g_location	east_coordinate
-Coordinates/Elevation_Meters								mapped to	NONE
-"""
+## The structure is as follows
 
-	if "Coordinates" in row:
-		for coor in row["Coordinates"]:
-			if "Standard" in coo:
-				coor_standard_latitude_decimal = verify_key(coor["Standard"]["Latitude"], "Decimal")
-				#print (coor_standard_latitude_decimal)
-				coor_standard_longitude_decimal = verify_key(coor["Standard"]["Longitude"], "Decimal")
-				#print (coor_standard_longitude_decimal)
-			if "Bounding" in coo:
-				coor_bounding_latitude_least = verify_key(coor["Bounding"]["Latitude"]["Least"], "Decimal")
-				#print (coor_bounding_latitude_least)
-				coor_bounding_latitude_most = verify_key(coor["Bounding"]["Latitude"]["Most"], "Decimal")
-				#print (coor_bounding_latitude_most)
-				coor_bounding_longitude_least = verify_key(coor["Bounding"]["Longitude"]["Least"], "Decimal")
-				#print (coor_bounding_longitude_least)
-				coor_bounding_longitude_most = verify_key(coor["Bounding"]["Longitude"]["Most"], "Decimal")
-				#print (coor_bounding_longitude_most)
-			coor_elevation_meters = verify_key(coor, "Elevation_Meters")
-			#print (coor_elevation_meters)
-			
-"""
-To extract Descriptive Notes if they exist
-
-The structure is as follows
-
-Descriptive_Note/Note_Text
-Descriptive_Note/Note_Contributors/Note_Contributors/Note_Contributor/Contributor_id
-Descriptive_Note/Note_Sources/Note_Source/Source/Source_ID
-Descriptive_Note/Note_Sources/Note_Source/Source/Brief_Citation
-Descriptive_Note/Note_Sources/Note_Source/Source/Full_Citation
-Descriptive_Note/Note_Sources/Note_Source/Source/Biblio_Note
-Descriptive_Note/Note_Sources/Note_Source/Source/Merged_Status
-Descriptive_Note/Note_Sources/Note_Source/Page
-"""
-
-"""
-To extract Parent Relationships if they exist
-
-The structure is as follows
-
-Parent_Relationships/Preferred_Parent/Parent_Subject_ID
-Parent_Relationships/Preferred_Parent/Relationship_Type
-Parent_Relationships/Preferred_Parent/Historic_Flag
-Parent_Relationships/Preferred_Parent/Parent_Date
-Parent_Relationships/Non_Preferred_Parent/Parent_Subject_ID
-Parent_Relationships/Non_Preferred_Parent/Relationship_Type
-Parent_Relationships/Non_Preferred_Parent/Historic_Flag
-Parent_Relationships/Non_Preferred_Parent/Parent_Date
-"""
-
-"""
-To extract Place Types if they exist
-
-The structure is as follows
-
-Place_Types/Preferred_Place_Type/Place_Type_ID
-Place_Types/Preferred_Place_Type/Display_Order
-Place_Types/Preferred_Place_Type/Historic_Flag
-Place_Types/Preferred_Place_Type/PT_Date
-Place_Types/Non_Preferred_Place_Type/Place_Type_ID
-Place_Types/Non_Preferred_Place_Type/Display_Order
-Place_Types/Non_Preferred_Place_Type/Historic_Flag
-Place_Types/Non_Preferred_Place_Type/PT_Date
-"""
+## Coordinates/Standard/Latitude/Decimal                       mapped to	g_location	north_coordinate + south_coordinate
+## Coordinates/Standard/Longitude/Decimal                      mapped to	g_location	east_coordinate + west_coordinate
+## Coordinates/Bounding/Latitude/Latitude_Least/Decimal        mapped to	g_location	south_coordinate
+## Coordinates/Bounding/Latitude/Latitude_Most/Decimal         mapped to	g_location	north_coordinate
+## Coordinates/Bounding/Longitude/Longitude_Least/Decimal      mapped to	g_location	west_coordinate
+## Coordinates/Bounding/Longitude/Longitude_Most/Decimal       mapped to	g_location	east_coordinate
+## Coordinates/Elevation_Meters                                mapped to	NONE
 
 
-	
-"""
-Let's now insert the values extracted from the XML into the database
-"""
-##	inserting a new collection and getting its id
-	c.execute("INSERT INTO table g_collection (name) VALUES ('TGN');")
-	collection_id = c.execute(" select collection_id from g_collection where name = 'TGN';")
+    if "Coordinates" in row:
+        for coor in row["Coordinates"]:
+            if "Standard" in coor:
+                coor_standard_latitude_decimal = verify_key(coor["Standard"]["Latitude"], "Decimal")
+                #print (coor_standard_latitude_decimal)
+                coor_standard_longitude_decimal = verify_key(coor["Standard"]["Longitude"], "Decimal")
+                #print (coor_standard_longitude_decimal)
+            if "Bounding" in coor:
+                coor_bounding_latitude_least = verify_key(coor["Bounding"]["Latitude"]["Least"], "Decimal")
+                #print (coor_bounding_latitude_least)
+                coor_bounding_latitude_most = verify_key(coor["Bounding"]["Latitude"]["Most"], "Decimal")
+                #print (coor_bounding_latitude_most)
+                coor_bounding_longitude_least = verify_key(coor["Bounding"]["Longitude"]["Least"], "Decimal")
+                #print (coor_bounding_longitude_least)
+                coor_bounding_longitude_most = verify_key(coor["Bounding"]["Longitude"]["Most"], "Decimal")
+                #print (coor_bounding_longitude_most)
+            coor_elevation_meters = verify_key(coor, "Elevation_Meters")
+            #print (coor_elevation_meters)
 
-##	inserting a new feature using the collection_id as FK and getting the feature_id to use afterwards
-	c.execute("INSERT INTO table g_feature (collection_id) VALUES (", g_collection_id, '"')
-	feature_id = c.execute("select feature_id from g_feature order by feature_id desc limit 1")
+
+###
+##To extract Descriptive Notes if they exist
+
+##The structure is as follows
+
+##Descriptive_Note/Note_Text
+##Descriptive_Note/Note_Contributors/Note_Contributors/Note_Contributor/Contributor_id
+##Descriptive_Note/Note_Sources/Note_Source/Source/Source_ID
+##Descriptive_Note/Note_Sources/Note_Source/Source/Brief_Citation
+##Descriptive_Note/Note_Sources/Note_Source/Source/Full_Citation
+##Descriptive_Note/Note_Sources/Note_Source/Source/Biblio_Note
+##Descriptive_Note/Note_Sources/Note_Source/Source/Merged_Status
+##Descriptive_Note/Note_Sources/Note_Source/Page
 
 
-		#print ("item inserted \n")
+###
+##To extract Parent Relationships if they exist
+
+##The structure is as follows
+
+##Parent_Relationships/Preferred_Parent/Parent_Subject_ID
+##Parent_Relationships/Preferred_Parent/Relationship_Type
+##Parent_Relationships/Preferred_Parent/Historic_Flag
+##Parent_Relationships/Preferred_Parent/Parent_Date
+##Parent_Relationships/Non_Preferred_Parent/Parent_Subject_ID
+##Parent_Relationships/Non_Preferred_Parent/Relationship_Type
+##Parent_Relationships/Non_Preferred_Parent/Historic_Flag
+##Parent_Relationships/Non_Preferred_Parent/Parent_Date
+
+
+###
+##To extract Place Types if they exist
+
+##The structure is as follows
+
+##Place_Types/Preferred_Place_Type/Place_Type_ID
+##Place_Types/Preferred_Place_Type/Display_Order
+##Place_Types/Preferred_Place_Type/Historic_Flag
+##Place_Types/Preferred_Place_Type/PT_Date
+##Place_Types/Non_Preferred_Place_Type/Place_Type_ID
+##Place_Types/Non_Preferred_Place_Type/Display_Order
+##Place_Types/Non_Preferred_Place_Type/Historic_Flag
+##Place_Types/Non_Preferred_Place_Type/PT_Date
+##
+
+
+
+
+## Let's now insert the features extracted from the XML into the database
+
+
+
+##  inserting a new feature using the collection_id as FK and getting the feature_id to use afterwards
+    #c.execute("INSERT INTO table g_feature (collection_id) VALUES (", g_collection_id, '"')
+    #feature_id = c.execute("select feature_id from g_feature order by feature_id desc limit 1")
+
+
