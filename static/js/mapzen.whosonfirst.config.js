07abd287f8118aac1c8f0fdd6373021aa7705a98
Bruno Martins <bgmartins@users.noreply.github.com>
2018-10-01 00:01:01 +0100

    Update mapzen.whosonfirst.config.js

--- static/js/mapzen.whosonfirst.config.js
+++ static/js/mapzen.whosonfirst.config.js
@@ -4,12 +4,12 @@ mapzen.whosonfirst.config = (function() {
 	var self = {
 
 		'init': function(){
-			mapzen.whosonfirst.data.endpoint("http://localhost:9999/");
-			mapzen.whosonfirst.pip.endpoint("http://localhost:8080/");
+			mapzen.whosonfirst.data.endpoint("/~gazetteer/index.wsgi/gazetteer-data/");
+			mapzen.whosonfirst.pip.endpoint("/~gazetteer/index.wsgi/pip/");
 			mapzen.whosonfirst.iamhere.scenefile("/~gazetteer/index.wsgi/static/tangram/simple.yaml");
 			mapzen.whosonfirst.leaflet.tangram.set_api_key("3XqXMjEdT2StnrIRJ4HYbg");
 			// general debugging
-			// mapzen.whosonfirst.log.show();
+			mapzen.whosonfirst.log.show();
 		}
 	};
 	return self
