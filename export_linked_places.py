8e7c5bc4d71b212603681301c007093f9d102c16
root <root@digch.novalocal>
2018-09-30 16:22:38 +0000

    Small changes in the linked places export script.

--- export_linked_places.py
+++ export_linked_places.py
@@ -1,4 +1,5 @@
 import os
+import json
 import sqlite3
 
 base_data = {
@@ -11,11 +12,21 @@ def export_gazetteer_to_linked_places( database ):
   data = base_data
   if not(os.path.isabs(database)): database = os.path.join(os.path.dirname(__file__),database)
   conn = sqlite3.connect( database )
-  c = conn.cursor()
-  for feature in c.execute("SELECT feature_id FROM g_feature WHERE collection_id is NOT NULL"):
-    feature = { "@id": "https://github.com/bgmartins/historical-gazetteer/" + os.path.basename(database) + "/" + repr(feature[0]), "type": "Feature", "properties":{}, "geometries": [], "when": {}, "names": [], "types": [], "relations": [], "links": [], "descriptions": [], "depictions": [] }
-    data["features"].append(feature)
+  for feature in conn.cursor().execute("SELECT DISTINCT feature_id FROM g_feature WHERE collection_id is NOT NULL"):
+    feature_obj = { "@id": "https://github.com/bgmartins/historical-gazetteer/" + os.path.basename(database) + "/" + repr(feature[0]), "type": "Feature", "properties":{}, "geometry": { "type": "GeometryCollection", "geometries": [] }, "when": {}, "names": [], "types": [], "relations": [], "links": [], "descriptions": [], "depictions": [] }
+    for name in conn.cursor().execute("SELECT name, language_code FROM g_feature_name, l_language WHERE g_feature_name.language_id=l_language.language_id AND feature_id=" + repr(feature[0])):
+      toponym = { "toponym": name[0], "lang": name[1], "citation": [], "when": [] }
+      feature_obj["names"].append(toponym)    
+    for geo in conn.cursor().execute("SELECT west_coordinate, south_coordinate FROM g_location LEFT JOIN g_location_geometry ON g_location.location_id=g_location_geometry.location_id WHERE g_location.feature_id=" + repr(feature[0])):
+      wkt = "POINT(" + repr(geo[0]) + " " + repr(geo[1]) + ")"
+      geo = { "type": "Point", "coordinates": [geo[0],geo[1]], "geo_wkt": wkt, "when": [], "src": "" }
+      feature_obj["geometry"]["geometries"].append(geo)    
+    for type in conn.cursor().execute("SELECT feature_code_id, l_scheme_term.term FROM g_feature_code, l_scheme_term WHERE l_scheme_term.scheme_id=g_feature_code.code_scheme_id AND l_scheme_term.scheme_term_id=g_feature_code.feature_code_id AND g_feature_code.feature_id=" + repr(feature[0])):
+      type_obj = { "identifier": type_obj[0], "label": type_obj[1], "sourceLabel": type_obj[1], "when": [] }
+      feature_obj["types"].append(type_obj)
+    data["features"].append(feature_obj)
   return data
 
 if __name__ == '__main__':
-  print(export_gazetteer_to_linked_places('gazetteer.db'))
+  data = export_gazetteer_to_linked_places('gazetteer.db')
+  print(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')))
