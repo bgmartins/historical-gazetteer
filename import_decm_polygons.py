e9d104a78eb8ec6f8b6333494723e4c3aa893765
Jacinto Estima <jacinto.estima@gmail.com>
2019-02-21 00:06:18 +0000

    created a function to get the latest id

--- import_decm_polygons.py
+++ import_decm_polygons.py
@@ -10,6 +10,13 @@ if not(os.path.isabs(database)): database = os.path.join(os.path.dirname(__file_
 conn = sqlite3.connect( database )
 #conn.execute("INSERT INTO g_collection VALUES (1, 'DECM Data', '')")
 
+def get_latest_id(table, id_field):
+    last_id = conn.execute("SELECT MAX({fld}) from {tbl};".format(tbl=table,fld=id_field)).fetchone()
+    if last_id is None or last_id[0] is None: last_id = 0
+    else: last_id = last_id[0] + 1
+    return last_id
+
+
 def import_from_shapefile( shp_path , attribute_name , source_desc , feature_type , date_desc ): 
     data = geopandas.read_file(shp_path, encoding='utf8')
     for index, row in data.iterrows():
@@ -21,21 +28,10 @@ def import_from_shapefile( shp_path , attribute_name , source_desc , feature_typ
         time_pediod_id = conn.execute("SELECT g_time_period_to_period_name.time_period_id FROM l_time_period_name, g_time_period_to_period_name WHERE l_time_period_name.time_period_name_id=g_time_period_to_period_name.time_period_name_id AND time_period_name=?",(date_desc,)).fetchone()[0]
         language_id  = conn.execute("SELECT language_id FROM l_language WHERE language_code LIKE 'SPA'").fetchone()[0]
 
-        feature_id  = conn.execute("SELECT MAX(feature_id) FROM g_feature").fetchone()        
-        if feature_id is None: feature_id = 0
-        else: feature_id = feature_id[0] + 1
-
-        feature_name_id  = conn.execute("SELECT MAX(feature_name_id) FROM g_feature_name").fetchone() 
-        if feature_name_id is None: feature_name_id = 0
-        else: feature_name_id = feature_name_id[0] + 1
-
-        location_id  = conn.execute("SELECT MAX(location_id) FROM g_location").fetchone()
-        if location_id is None: location_id = 0
-        else: location_id = location_id[0] + 1
-
-        location_geometry_id = conn.execute("SELECT MAX(location_geometry_id) FROM g_location_geometry").fetchone() 
-        if location_geometry_id is None or location_geometry_id[0] is None: location_geometry_id = 0
-        else: location_geometry_id = location_geometry_id[0] + 1
+        feature_id = get_latest_id("g_feature","feature_id")
+        feature_name_id = get_latest_id("g_feature_name","feature_name_id")
+        location_id = get_latest_id("g_location","location_id")
+        location_geometry_id = get_latest_id("g_location_geometry", "location_geometry_id")
                         
         conn.execute("INSERT INTO g_feature ( feature_id , collection_id , is_complete , time_period_id , entry_note , entry_date , modification_date ) VALUES (?,?,?,?,?,'now','now')", ( feature_id, 1, True, time_pediod_id, source_desc ) )
         conn.execute("INSERT INTO g_feature_name ( feature_name_id , feature_id , primary_display , language_id , transliteration_scheme_id , name ) VALUES (?,?,?,?,9,?)", ( feature_name_id, feature_id, True, language_id, name ) )
